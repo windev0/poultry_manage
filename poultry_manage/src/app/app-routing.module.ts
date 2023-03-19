fb252d8adf0f80867c75a68c79fd7e2ad7f98690
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-03-19 22:06:59 +0000

    routing entre la page d'accueil et la page de connexion

--- poultry_manage/src/app/app-routing.module.ts
+++ poultry_manage/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path : "login", component: LoginComponent},
-  // {path : "login", component: LoginComponent},
+  {path : "accueil", component: AppComponent},
 
 ];
 
--- poultry_manage/src/app/login/login.component.ts
+++ poultry_manage/src/app/login/login.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  showSidebar = true;
+  
+  toggleSidebar() {
+    this.showSidebar = !this.showSidebar;
+  }
 }
