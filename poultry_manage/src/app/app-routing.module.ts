df2a2baf4618bbbafad3691a62b6433ae61ab417
AWOUNO Kossi Winner <kossiwinnerawouno@gmail.com>
2023-03-20 00:20:10 +0000

    création des composants oeuf, vente, rapport

--- poultry_manage/src/app/app-routing.module.ts
+++ poultry_manage/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { PouleComponent } from './poule/poule.component';
 
 const routes: Routes = [
   {path : "login", component: LoginComponent},
   {path : "accueil", component: AppComponent},
-  {path : "poule", component: AppComponent},
+  {path : "poule", component: PouleComponent},
 ];
 
 @NgModule({
--- poultry_manage/src/app/app.module.ts
+++ poultry_manage/src/app/app.module.ts
@@ -14,12 +14,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PouleComponent } from './poule/poule.component';
+import { OeufComponent } from './oeuf/oeuf.component';
+import { VenteComponent } from './vente/vente.component';
+import { RapportComponent } from './rapport/rapport.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PouleComponent
+    PouleComponent,
+    OeufComponent,
+    VenteComponent,
+    RapportComponent
   ],
   imports: [
     BrowserModule,
